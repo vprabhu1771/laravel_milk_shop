17ba78ec7e860021085db71c3cfc5d0c1e2b73a3
vprabhu1771 <vprabhu1771@gmail.com>
2025-02-16 10:49:57 +0530

    ProductResource variants Repeater added

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -31,6 +31,18 @@ class ProductResource extends Resource
                     ->default('no_image_available.jpg'),
                 Forms\Components\Textarea::make('description')
                     ->columnSpanFull(),
+                Forms\Components\Repeater::make('variants')
+                    ->relationship()
+                    ->schema([                        
+                        Forms\Components\TextInput::make('qty'),
+                        Forms\Components\TextInput::make('unit_price')->required(),
+                    ])
+                    ->reorderable(true)
+                    ->reorderableWithButtons()
+                    // ->collapsible()
+                    ->cloneable()
+                    ->columns(2)
+                    ->columnSpanFull(),
             ]);
     }
 
